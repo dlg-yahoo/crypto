089dae4dc34b24b5693797bb40aea087fd5018e1
David Leon Gil <dgil@yahoo-inc.com>
2014-12-29 18:33:36 -0800

    Some more work on non-entropy-requiring ECDSA.

---
+++ src/crypto/ecdsa/util.go
@@ -0,0 +1,18 @@
+package ecdsa
+
+import (
+	"io"
+)
+
+type zr struct {
+	io.Reader
+}
+
+func (z *zr) Read(dst []byte) (n int, err error) {
+	for i := range dst {
+		dst[i] = 0
+	}
+	return len(dst), nil
+}
+
+var zeroReader = &zr{}
